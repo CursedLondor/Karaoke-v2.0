b5a4f4902aca55e305bbc1726caabcc893190444
CursedLondor <musluca@virgilio.it>
2018-10-08 18:57:02 +0200

    Added duet functionality

--- KaraokeImbarazzante.py
+++ KaraokeImbarazzante.py
@@ -10,8 +10,15 @@ with open('./Invitati.json') as singers:
 while True:
     randomsong = random.randint(0,len(songs)-1)
     numberofsingers = int(songs[randomsong]["numberofsingers"])
-    theyhavetosing = random.sample(singers, numberofsingers)
-    print("Autore: " + songs[randomsong]["author"] + "\tCanzone: " + songs[randomsong]["name"])
+    if(numberofsingers==2 and songs[randomsong]["sex"]=="MF"):
+        boolean = True
+        while boolean:
+            theyhavetosing = random.sample(singers, numberofsingers)
+            if((theyhavetosing[0]["sex"] == "M" and theyhavetosing[1]["sex"] == "F") or (theyhavetosing[0]["sex"] == "F" and theyhavetosing[1]["sex"] == "M")):
+                boolean = False
+    else :
+        theyhavetosing = random.sample(singers, numberofsingers)
+    print("Autore: " + songs[randomsong]["author"] + "\tCanzone: " + songs[randomsong]["name"] + "\n")
     print("Cantanti:")
     for number in range(0, numberofsingers):
         print (theyhavetosing[number]["name"])
