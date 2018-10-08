5fb4e95bea3123675f0bb2b455b9353bdf8fc16e
CursedLondor <musluca@virgilio.it>
2018-10-08 20:44:15 +0200

    Added Pop functionality
    Now, two new files will be created. These files contains remaining songs and singers

--- KaraokeImbarazzante.py
+++ KaraokeImbarazzante.py
@@ -10,12 +10,20 @@ with open('./Invitati.json') as singers:
 while True:
     randomsong = random.randint(0,len(songs)-1)
     numberofsingers = int(songs[randomsong]["numberofsingers"])
-    if(numberofsingers==2 and songs[randomsong]["sex"]=="MF"):
+    if numberofsingers==2 and songs[randomsong]["sex"]=="MF" :
         boolean = True
+        x = 1
         while boolean:
+            if len(singers) < numberofsingers :
+                print("Non ci sono abbastanza cantanti!")
+                exit(0)
             theyhavetosing = random.sample(singers, numberofsingers)
-            if((theyhavetosing[0]["sex"] == "M" and theyhavetosing[1]["sex"] == "F") or (theyhavetosing[0]["sex"] == "F" and theyhavetosing[1]["sex"] == "M")):
+            if (theyhavetosing[0]["sex"] == "M" and theyhavetosing[1]["sex"] == "F") or (theyhavetosing[0]["sex"] == "F" and theyhavetosing[1]["sex"] == "M") :
                 boolean = False
+            x = x+1
+            if x == 30 :
+                print("Non trovo cantanti per un duetto!")
+                exit(0)
     else :
         theyhavetosing = random.sample(singers, numberofsingers)
     print("Autore: " + songs[randomsong]["author"] + "\tCanzone: " + songs[randomsong]["name"] + "\n")
@@ -23,4 +31,17 @@ while True:
     for number in range(0, numberofsingers):
         print (theyhavetosing[number]["name"])
     print("\n\n\n")
+    songs.pop(randomsong)
+    for num in range(0, len(theyhavetosing)):
+        singers.pop(singers.index(theyhavetosing[num]))
+    with open('CanzoniRestanti.json', 'w') as remainingsongs:
+        json.dump(songs, remainingsongs)
+    with open('InvitatiRestanti.json', 'w') as remainingsingers:
+        json.dump(singers, remainingsingers)
+    if len(songs) == 0 :
+        print("Le canzoni sono finite!")
+        exit(0)
+    if len(singers) == 0 :
+        print("I cantanti sono finiti!")
+        exit(0)
     raw_input()
